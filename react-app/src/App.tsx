b167be7219650d18a6c6d0a8fc4fefb8c4a4f58b
Hasan Naqvi <hasannaqvi@live.com>
2019-10-23 00:00:23 +1100

    dev

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Hello } from 'spfx-library';
+import styles from './App.module.scss';
+
 import './css/Test.css';
 
 const App: React.FC = () => {
@@ -12,6 +14,12 @@ const App: React.FC = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h3>This a sample React Application built using create-react-app CLI</h3>
         <Hello title={'from SPFx Library'} />
+        <br />
+        <div className={styles.AppSCSS} >
+          <div className={styles.Hello} >
+            Hello from SCSS defined locally in App
+          </div>
+        </div>
         <div className='Test'>
           <span className="testScss">Test SCSS from within React App</span>
         </div>
